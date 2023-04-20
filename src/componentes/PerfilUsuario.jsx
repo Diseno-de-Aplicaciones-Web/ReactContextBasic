f29a20d68e74b55b5a842f1146a948792ebeefc2
DanBaDo <byte4life@pm.me>
2023-04-20 17:43:15 +0200

    Resolves TODO

--- src/componentes/PerfilUsuario.jsx
+++ src/componentes/PerfilUsuario.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react"
+import { Contexto } from "../App"
 import CerrarSesion from "./CerrarSesion"
 
 function PerfilUsuario() {
+
+    const [ usuario ] = useContext(Contexto)
+
     return(
         <div>
-            <h3>Perfil de {"TODO: usuario"}</h3>
+            <h3>Perfil de {usuario}</h3>
             <CerrarSesion/>
         </div>
     )
